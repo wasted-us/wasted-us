51d048efcfb6ab8e3d8d5e3d58d973e12e4bfb4f
Kevin <kevin.sweet@sendgrid.com>
2014-06-26 14:08:19 -0600

    added form

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,16 +1,23 @@
 $(function() {
-  var counter = 0;
-  var counter_display;
-  var num_people = 10;
-  var salary_per_second = 100000 / ( 2000 * 60 * 60 );
+  $('#main-form').on('submit', function(event) {
+    event.preventDefault();
 
-  $('#flipcountdown').flipcountdown({
-    size:"lg",
-    tick: function() {
-      counter += (num_people * salary_per_second);
-      counter_display = parseFloat(counter).toFixed(2);
+    $(this).hide();
 
-      return counter_display;
-    }
+    var counter = 0;
+    var counter_display;
+    var num_people = $('#num-people').val() || 0;
+    var average_salary = $('#salary').val();
+    var salary_per_second = average_salary / ( 2000 * 60 * 60 );
+
+    $('#flipcountdown').flipcountdown({
+      size:"lg",
+      tick: function() {
+        counter += (num_people * salary_per_second);
+        counter_display = parseFloat(counter).toFixed(2);
+
+        return counter_display;
+      }
+    });
   });
 });

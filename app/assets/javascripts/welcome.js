63946efacfa02a6e5d0c669bd61d413eb83763ac
Kevin <kevin.sweet@sendgrid.com>
2014-06-26 13:19:39 -0600

    updated formula for counter

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,9 +1,16 @@
 $(function() {
-  var i = 0;
+  var counter = 0;
+  var counter_display;
+  var num_people = 10;
+  var salary_per_second = 100000 / ( 2000 * 60 * 60 );
+
   $('#flipcountdown').flipcountdown({
     size:"lg",
     tick: function() {
-      return i++;
+      counter += (num_people * salary_per_second);
+      counter_display = parseFloat(counter).toFixed(2);
+
+      return counter_display;
     }
   });
 });

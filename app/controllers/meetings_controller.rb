efeda0b88cc31d98404fa182f1f4fd662c1fbaad
Monica Mishra <monicamishra@SendGrids-MacBook-Pro-2.local>
2014-06-27 09:48:44 -0600

    Changed default participants to be zero

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -26,7 +26,8 @@ class MeetingsController < ApplicationController
   # POST /meetings.json
   def create
     @meeting = Meeting.new(meeting_params)
-    @meeting.participant_count = 0
+    @meeting.checked_out_participant_count = 0
+    @meeting.participant_count ||= 0
     respond_to do |format|
       if @meeting.save
         format.html { redirect_to @meeting, notice: 'Meeting was successfully created.' }
